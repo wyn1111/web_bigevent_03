65b5f6f76d73e22555bdb31c6118783bcf943f5b
wyn <2335627693@qq.com>
2021-01-18 21:41:40 +0800

    完成了登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -55,7 +55,6 @@ $(function () {
             url:'/api/login',
             data:$(this).serialize(),
             success:function(res){
-                // console.log(res);
                 if(res.status !== 0){
                     return layer.msg(res.message)
                 }
